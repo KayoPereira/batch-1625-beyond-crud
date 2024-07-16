979acc47b42cb9865f734b51fe93e6efd6d5d9f0
Kayo Pereira <kayo.richard2000@gmail.com>
2024-07-16 20:32:28 -0300

    add review crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   resources :restaurants do
     collection do
       get :top
-      get :chef
     end
     # get "/chef", to: "restaurants#chef"
 
     member do
       get :chef
     end
+
+    # /restaurants/:id/reviews/new
+    resources :reviews, only: [ :new, :create ]
   end
+
+  resources :reviews, only: [ :destroy ]
+
+  # /restaurants/:restaurant_id/reviews/:review_id/banana/new
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
